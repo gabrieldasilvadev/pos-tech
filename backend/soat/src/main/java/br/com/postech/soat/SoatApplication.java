1f4bf3c0c27b7208a841b4a7dfb085f809a155b7
gabrieldasilvadev <gabrieldasilvadev@gmail.com>
2025-04-26 13:22:18 -0300

    Add initial project configuration files and update .gitignore

---
+++ backend/soat/src/main/java/br/com/postech/soat/SoatApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.soat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoatApplication.class, args);
+  }
+
+}
