d9e0e1fd97bb2943ffb4da14743b583b076b8781
gabrieldasilvadev <gabrieldasilvadev@gmail.com>
2025-05-02 03:18:47 -0300

    Remove unused TesteApi class

--- backend/soat/src/main/java/br/com/postech/soat/cliente/adapters/TesteApi.java
+++
@@ -1,9 +0,0 @@
-package br.com.postech.soat.cliente.adapters;
-
-
-import br.com.postech.soat.openapi.api.CreditSimulationApi;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TesteApi  implements CreditSimulationApi {
-}
