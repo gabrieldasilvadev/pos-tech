81cd2504d816aed5dc16b71d9927e14e82157793
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2025-05-06 00:07:28 -0300

    🗃️ create transaction/method/status tables
    - transaction_method enum (only QRCODE);
    - transaction_status enum (PENDING, PAID, FAILED);
    - transaction created/processed/updated_at timestamps;
    - transaction value must be greater than zero;
    - order_id with cascade on delete;

---
+++ backend/soat/src/main/resources/db/migration/postgresql/V202505052305__create-table-transaction.sql
@@ -0,0 +1,24 @@
+-- create payment_method table
+CREATE TYPE payment_method AS ENUM (
+    'QRCODE'
+);
+
+-- create transaction_status table
+CREATE TYPE transaction_status AS ENUM (
+    'PENDING',
+    'PAID',
+    'FAILED'
+);
+
+-- create transaction table
+CREATE TABLE transaction (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    method payment_method NOT NULL,
+    order_id UUID NOT NULL REFERENCES "order"(id) ON DELETE CASCADE,
+    qr_code TEXT NOT NULL,
+    value NUMERIC(10, 2) NOT NULL CHECK (value > 0),
+    status transaction_status NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE
+);
