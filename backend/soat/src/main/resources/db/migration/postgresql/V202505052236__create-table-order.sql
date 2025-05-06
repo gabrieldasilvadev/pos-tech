304fd5e5aadb3098f51281fef1b14594409997bb
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2025-05-05 23:59:27 -0300

    🗃️ create order/order_item tables
    - order table partitioned by created_at (improve performance);
    - included some checks to prices/amounts (greater than [or equal to]
     zero);
    - 'product_name' field denormalized for historical accuracy in
    order_item table;

---
+++ backend/soat/src/main/resources/db/migration/postgresql/V202505052236__create-table-order.sql
@@ -0,0 +1,22 @@
+-- create order table
+CREATE TABLE "order" (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    customer_id UUID NOT NULL REFERENCES customer(id),
+    total_price NUMERIC(10, 2) NOT NULL CHECK (total_price > 0),
+    discount NUMERIC(10, 2) DEFAULT 0 CHECK (discount >= 0),
+    observation VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+) PARTITION BY RANGE (created_at);
+
+-- create order_item table
+CREATE TABLE order_item (
+    id UUID PRIMARY KEY DEFAULT en_random_uuid(),
+    order_id UUID NOT NULL REFERENCES "order"(id) ON DELETE CASCADE,
+    product_id UUID NOT NULL REFERENCES product(id),
+    product_name VARCHAR(255) NOT NULL,  -- Denormalized for historical accuracy
+    product_quantity INTEGER NOT NULL CHECK (product_quantity > 0),
+    unit_price NUMERIC(10, 2) NOT NULL CHECK (unit_price > 0),
+    discount NUMERIC(10, 2) DEFAULT 0 CHECK (discount >= 0),
+    observation VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+);
