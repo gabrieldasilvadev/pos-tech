5b25204a74d32440d09c7329d4f637fa01e2419e
gabrieldasilvadev <gabrieldasilvadev@gmail.com>
2025-05-25 15:06:10 -0300

    fix(payment): Improve payment processing logic and logging

--- backend/soat/src/main/java/br/com/postech/soat/payment/core/application/services/ReprocessPendingPayments.java
+++ backend/soat/src/main/java/br/com/postech/soat/payment/core/application/services/ReprocessPendingPayments.java
@@ -29,10 +29,13 @@ public class ReprocessPendingPayments {
             if (result == GatewayOperationResult.FAILURE) {
                 logger.error("Payment processing failed for payment ID: {}", payment.getId());
                 payment.fail();
-            } else {
+                paymentRepository.save(payment);
                 payment.approve();
-                logger.info("Payment processed successfully for payment ID: {}", payment.getId());
+                return;
             }
+
+            payment.approve();
+            logger.info("Payment processed successfully for payment ID: {}", payment.getId());
 
             paymentRepository.save(payment);
         }
